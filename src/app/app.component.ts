e1ed872f6c908219a2781f01cfc793902396497d
AkiroSetonai <wallyson.cavalcante@gmail.com>
2025-03-23 10:12:31 -0300

    feat users display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,23 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
 
   onSelectUser(id: string) {
-    console.log(`Usuario selecionado com a id ${id}`);
+    this.selectedUserId = id;
   }
 }
