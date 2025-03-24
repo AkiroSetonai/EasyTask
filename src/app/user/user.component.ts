b52db561ba44393d969817a9cd994044957e84c9
AkiroSetonai <wallyson.cavalcante@gmail.com>
2025-03-24 11:47:45 -0300

    feat: css dinamico

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
-// type User = {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// }
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { type User } from './user.model';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -19,6 +8,7 @@ interface User {
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
